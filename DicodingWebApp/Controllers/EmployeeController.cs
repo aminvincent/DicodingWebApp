a3a93261fb8c4909286b580e68aa453d2dddfece
Amin Vincent <aminvincent@outlook.com>
2019-06-25 11:03:07 +0700

    Add project files.

---
+++ DicodingWebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using DicodingWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DicodingWebApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            using (dicodingdbEntities db = new dicodingdbEntities())
+            {
+                var query = db.Employee.Select(x => new EmployeeModel
+                {
+                    EmployeeId = x.EmployeeId,
+                    Name = x.Name,
+                    Email = x.Email,
+                    Job = x.Job
+                }).ToList();
+
+                ViewBag.EmployeeList = query;
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ DicodingWebApp/Models/EmployeeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DicodingWebApp.Models
+{
+    public class EmployeeModel
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Job { get; set; }
+    }
+}
